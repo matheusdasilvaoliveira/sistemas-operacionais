a0b2bc612ba8a1b8a495fcc22963a79bce21c03f
Matheus Oliveira <133789002+matheusdasilvaoliveira@users.noreply.github.com>
2026-03-31 13:29:46 +0000

    faz um filhocidio com for_ever

---
+++ labs/lab05/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#define EVER ;;
+
+void intHandler(int sinal);
+void quitHandler(int sinal);
+
+int main (void) {
+    void (*p)(int);// ponteiro para função que recebe int como parâmetro
+    p = signal(SIGINT, intHandler);
+    printf("Endereco do manipulador anterior %p\n", p);
+    p = signal(SIGQUIT, quitHandler);
+    printf("Endereco do manipulador anterior %p\n", p);
+    printf ("Ctrl-C desabilitado. Use Ctrl-\\ para terminar");
+    for(EVER);
+}
+
+void intHandler(int sinal) {
+    printf("\nVocê pressionou Ctrl-C (%d)", sinal);
+    return;
+}
+
+void quitHandler(int sinal) {
+    puts ("\nTerminando o processo...");
+    exit (0);
+}
