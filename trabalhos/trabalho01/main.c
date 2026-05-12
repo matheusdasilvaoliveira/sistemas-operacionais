9634985be076a150b206576826aa69784942c162
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2026-05-12 13:08:48 +0000

    adicionando o esqueto do começo do trabalho

---
+++ trabalhos/trabalho01/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    int qntProcessos;
+
+    printf("Entre com a quantidade de processos entre 3 a 6: ");
+    scanf("%d", qntProcessos);
+
+    while(qntProcessos < 3 && qntProcessos > 6){
+
+
+        
+    }
+}
+
+void kernelSim(){
+
+}
+
+void interControllerSim(){
+
+}
+
+
