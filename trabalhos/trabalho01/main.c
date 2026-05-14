fe08638f1035bfdfc560f5e6afab2844c0fd782b
Matheus Oliveira <133789002+matheusdasilvaoliveira@users.noreply.github.com>
2026-05-14 13:32:27 +0000

    cria altera processo

--- trabalhos/trabalho01/main.c
+++ trabalhos/trabalho01/main.c
@@ -4,13 +4,12 @@
 #include <signal.h>
 #include <sys/types.h>
 
-int pid[3];
+// [[pid, pc da CPU], [pid, pc da CPU]]
+int processos[3][2];
+int qntProcessos = 3;
 
 void kernelSim(){
-       while(1){
-        printf("[kernel], em execução...\n");
-        sleep(1);
-    }
+   
 }
 
 void interControllerSim(){
@@ -20,9 +19,21 @@ void interControllerSim(){
     }
 }
 
-void processoAplicacao(int id){
-    int pc=0;
+void criaProcesso(int id, int *processo) {
+    int tempoCPU = processo[1];
 
+    processo[0] = fork();
+    processo[1] = 0;
+
+    if(processo[0]==0){
+        kill(processo[0], SIGSTOP); 
+        exit(0);
+    }
+
+    printf("Processo [A%d] criado | pc = %d\n", id, processo[1]);
+}
+
+void processoAplicacao(int id, int pc){
     while(1){
         printf("[A%d], em execução, pc = %d\n", id, pc);
         pc++;
@@ -31,17 +42,15 @@ void processoAplicacao(int id){
 }
 
 int main(){
-    int qntProcessos;
+    // printf("Entre com a quantidade de processos entre 3 a 6: ");
+    // scanf("%d", &qntProcessos);
 
-    printf("Entre com a quantidade de processos entre 3 a 6: ");
-    scanf("%d", &qntProcessos);
+    // while(qntProcessos < 3 || qntProcessos > 6){
 
-    while(qntProcessos < 3 || qntProcessos > 6){
-
-        printf("Valor invalido, digite entre 3 e 6: ");
-        scanf("%d", &qntProcessos);
+    //     printf("Valor invalido, digite entre 3 e 6: ");
+    //     scanf("%d", &qntProcessos);
         
-    }
+    // }
 
     // pid_t pid[qntProcessos];
 
@@ -52,33 +61,34 @@ int main(){
     //     exit(0);
     // }
 
-    pid_t processo = fork();
+    pid_t kernel = fork();
 
-    if(processo == 0){
-        kernelSim();
-        exit(0);
-    }
+    if(kernel == 0){
+        for(int i = 0; i<qntProcessos;i++){
+            criaProcesso((i + 1), processos[i]);
 
-    for(int i = 0; i<qntProcessos;i++){
-        pid[i] = fork();
+        }
+    
+        int atual=0;
 
-        if(pid[i]==0){
-            processoAplicacao(i+1);
-            exit(0);
+        while(1){
+            printf("[kernel], em execução...\n");
+            sleep(1);
+
+            kill(pid[atual], SIGCONT);
+            sleep(3);
+            kill(pid[atual], SIGSTOP);
+            atual = (atual+1)%qntProcessos;
+    
+            sleep(10);
         }
 
-        kill(pid[i],SIGSTOP); 
+            exit(0);
+        
     }
-    int atual=0;
 
-    while(1){
-        kill(pid[atual], SIGCONT);
-        sleep(3);
-        kill(pid[atual], SIGSTOP);
-        atual = (atual+1)%qntProcessos;
-   
-        sleep(10);
-    }
+    
+    
     return(1);
 }
 
